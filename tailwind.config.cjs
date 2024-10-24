fbb6c1e7b4e69975ab42d3b24a702adab989b985
Lucas Philippe <lucas.philippe.nunes@gmail.com>
2024-10-24 10:45:13 -0300

    new cards

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
+const cardUtilities = plugin(function ({ addUtilities }) {
+	addUtilities({
+		".rotateY-180": {
+			transform: "rotateY(180deg)",
+		},
+		".preserve-3d": {
+			transformStyle: "preserve-3d",
+		},
+		".perspective": {
+			perspective: "1000px",
+		},
+		".backface-hidden": {
+			backfaceVisibility: "hidden",
+		},
+	});
+});
+
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
-	plugins: [require("tailwind-scrollbar")],
+	plugins: [require("tailwind-scrollbar"), cardUtilities],
 	theme: {
 		extend: {},
 		colors: {
