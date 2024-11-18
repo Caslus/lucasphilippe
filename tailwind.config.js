395f032395288d77fe11a935a0b5ae6479de6d4e
Lucas Philippe <lucas.philippe.nunes@gmail.com>
2024-11-18 16:14:50 -0300

    style: switch styling to tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ export default {
 		extend: {
 			colors: {
 				"background-dark": "#0b0909",
+				"title-dark": "#d6d6d6",
+				"content-dark": "#8c8c8c",
+				"content-selection": "#a92e2f88",
+			},
+			screens: {
+				"2xsm": "376px",
+				xsm: "401px",
+				sm: "451px",
+				md: "801px",
 			},
 		},
 	},
