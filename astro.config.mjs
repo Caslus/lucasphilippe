4b3a973f4412d2826ff3022f74ec21c880400792
Lucas Philippe <lucas.philippe.nunes@gmail.com>
2024-11-18 14:24:15 -0300

    chore: add tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
+import tailwindIntegration from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [],
+	integrations: [tailwindIntegration()],
 	site: "https://lucasphilippe.com",
 });
