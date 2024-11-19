2022c5ef196b7eb1559bdf50fb63b7b02b8bbdb4
Lucas Philippe <lucas.philippe.nunes@gmail.com>
2024-11-18 22:34:15 -0300

    fix: dev routing

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
-export const url = "https://lucasphilippe.com";
+export const url = Bun.env.NODE_ENV === "development" ? "http://localhost:4321" : "https://lucasphilippe.com";
 export const github = "https://github.com/Caslus";
 export const linkedin = "https://www.linkedin.com/in/lucasphi/";
